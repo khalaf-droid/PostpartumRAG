ed034948e99be21b9fbf704df0405e38321cab42
Khalaf Hussein Khalaf Hussein <145412532+khalaf-droid@users.noreply.github.com>
2026-08-19 19:41:46 +0300

    fix environment production urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://postpartum-backend.onrender.com", // رابط الباك إند
-  ragUrl: "https://postpartum-rag-api.onrender.com", // رابط الـ RAG API
+  apiUrl: 'https://postpartum-backend.onrender.com/api',
+  ragUrl: 'https://postpartum-rag-api.onrender.com'
 };
